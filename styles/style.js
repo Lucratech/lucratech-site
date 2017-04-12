36423fa71b40b51572408cd4d9aaa225ae2898fe
Unknown <bradleyburnett@outlook.com>
2017-04-12 19:04:46 -0400

    fixed mobilemenu

--- styles/style.js
+++ styles/style.js
@@ -4,12 +4,13 @@ function myFunction(x) {
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function buttonfcn() {
+function buttonfcn(x) {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
+     x.classList.toggle("change");
   if (!event.target.matches('.dropbtn')) {
 
     var dropdowns = document.getElementsByClassName("dropdown-content");
